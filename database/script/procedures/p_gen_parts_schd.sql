3ffabf3ca7905f88cc20d050a4fed5d9f04332b8
326682792@qq.com <326682792@qq.com>
2020-04-03 11:55:56 +0800

    fsd

--- database/script/procedures/p_gen_parts_schd.sql
+++ database/script/procedures/p_gen_parts_schd.sql
@@ -5,18 +5,55 @@ DELIMITER //
 CREATE PROCEDURE p_gen_parts_schd(
                                  )
 BEGIN
-    CALL p_gen_parts('alarm');
-    CALL p_gen_parts('alarm_aux');
-    CALL p_gen_parts('alarm_others');
-    CALL p_gen_parts('nos_aa');
+    #Routine body goes here...
+    DROP TABLE IF EXISTS wv_tbs;
+    CREATE TEMPORARY TABLE wv_tbs
+    (
+        tbn VARCHAR(40)
+    ) ENGINE MEMORY;
 
-    CALL p_gen_parts('c3_sms');
-    CALL p_gen_parts('c3_sms_monitor');
-    CALL p_gen_parts('nos_ac');
+    INSERT INTO wv_tbs(tbn)
+    VALUES ('alarm')
+         , ('alarm_aux')
+         , ('alarm_others')
+         , ('nos_aa')
+         , ('c3_sms')
+         , ('c3_sms_monitor')
+         , ('nos_ac')
+         , ('stat_alarm_ex')
+         , ('stat_sms_ex');
 
-    CALL p_gen_parts('stat_alarm_ex');
-    CALL p_gen_parts('stat_sms_ex');
 
+    BEGIN
+        DECLARE v_tbn VARCHAR(40);
+        DECLARE v_CODE VARCHAR(5);
+        DECLARE v_msg TEXT;
+        DECLARE v_start DATETIME(6);
+        DECLARE v_end DATETIME(6);
+        DECLARE v_done BOOLEAN DEFAULT FALSE;
+        DECLARE cv_tbs CURSOR FOR SELECT tbn FROM wv_tbs;
+        DECLARE CONTINUE HANDLER FOR NOT FOUND SET v_done = TRUE;
+        DECLARE CONTINUE HANDLER FOR SQLEXCEPTION
+            BEGIN
+                GET DIAGNOSTICS CONDITION 1
+                    v_CODE = RETURNED_SQLSTATE,
+                    v_msg = MESSAGE_TEXT;
+            END;
+        OPEN cv_tbs;
+        floop:
+        BEGIN
+            FETCH cv_tbs INTO v_tbn;
+            IF v_done THEN
+                LEAVE floop;
+            END IF;
+            SET v_start = now(6);
+            CALL p_gen_parts(v_tbn);
+            SET v_end = now(6);
+            INSERT INTO event_exec_log (event_name, start_time, end_time, stat_code, msg)
+            VALUES ('part_alarm', v_start, v_end, v_CODE, v_msg);
+        END;
+
+    END;
 
 END //
 
