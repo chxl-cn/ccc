42e6000b5e075be15d69641909545d838636d36b
326682792@qq.com <326682792@qq.com>
2020-04-03 13:45:05 +0800

    datacenterlog

--- database/script/tables/datacenterlog.sql
+++ database/script/tables/datacenterlog.sql
@@ -1,16 +1,16 @@
 CREATE TABLE `datacenterlog`
 (
     `id`             VARCHAR(100) NOT NULL,
-    `user_name`      VARCHAR(100) DEFAULT NULL,
-    `module_name`    VARCHAR(100) DEFAULT NULL,
-    `client_ip`      VARCHAR(100) DEFAULT NULL,
+    `user_name`      VARCHAR(100),
+    `module_name`    VARCHAR(100),
+    `client_ip`      VARCHAR(100),
     `detail`         TEXT,
-    `log_time`       DATETIME     DEFAULT NULL,
-    `operation_name` VARCHAR(100) DEFAULT NULL,
-    `log_level`      INT(11)      DEFAULT NULL,
-    `server_ip`      VARCHAR(100) DEFAULT NULL,
+    `log_time`       DATETIME,
+    `operation_name` VARCHAR(100),
+    `log_level`      INT(11),
+    `server_ip`      VARCHAR(100),
     `fail_reason`    TEXT,
-    `is_succeed`     VARCHAR(100) DEFAULT NULL,
+    `is_succeed`     VARCHAR(100),
     PRIMARY KEY (`id`),
     KEY `ix_datacenterlog_user_name` (`user_name`)
 ) ENGINE = InnoDB
