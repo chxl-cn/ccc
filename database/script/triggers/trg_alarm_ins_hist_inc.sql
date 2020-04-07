52479f093834a76515031ece22004568f0a84e67
326682792@qq.com <326682792@qq.com>
2020-04-07 14:53:22 +0800

    add v_loco IS NULL

--- database/script/triggers/trg_alarm_ins_hist_inc.sql
+++ database/script/triggers/trg_alarm_ins_hist_inc.sql
@@ -6,16 +6,20 @@ CREATE TRIGGER `trg_alarm_ins_hist_inc`
     AFTER INSERT
     ON `alarm`
     FOR EACH ROW
+trg_body:
 BEGIN
     DECLARE v_loco VARCHAR(60);
     DECLARE v_rdt DATETIME;
 
     SET v_loco := new.detect_device_code;
+
+    IF v_loco IS NULL THEN
+        LEAVE trg_body ;
+    END IF;
+
     SET v_rdt := cast(new.raised_time AS DATE);
 
-    IF v_loco IS NOT NULL THEN
-        REPLACE alarm_hist_inc SET running_date=v_rdt, locomotive_code=v_loco ;
-    END IF;
+    REPLACE alarm_hist_inc SET running_date=v_rdt, locomotive_code=v_loco;
 
 
 END //
