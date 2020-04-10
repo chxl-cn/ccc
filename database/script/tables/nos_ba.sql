6e6eaed46789e6d2c4ae207862b210d8a63ffa00
326682792@qq.com <326682792@qq.com>
2020-04-10 16:40:06 +0800

    remove comment

--- database/script/tables/nos_ba.sql
+++ database/script/tables/nos_ba.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `nos_ba`
 (
     `ID`      varchar(128) NOT NULL,
-    `CONTENT` text COMMENT '' json ''
+    `CONTENT` text
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_0900_ai_ci
