72331f90e91dabb391fed9a246bc9525dd657926
326682792@qq.com <326682792@qq.com>
2020-04-13 09:45:09 +0800

    replace COMMENT '' json '' to ""

--- database/script/tables/nos_af.sql
+++ database/script/tables/nos_af.sql
@@ -2,7 +2,7 @@ CREATE TABLE `nos_af`
 (
     `ID`        varchar(128) NOT NULL,
     `INPUTDATE` datetime COMMENT '' date '',
-    `CONTENT`   text COMMENT '' json ''
+    `CONTENT`   text 
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_0900_ai_ci
