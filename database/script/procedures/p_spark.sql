dbe8d5ec8f50af6012c69c7e127501b6a8bc4ffa
326682792@qq.com <326682792@qq.com>
2020-04-21 13:49:23 +0800

    add wv_sms_alarm_out

--- database/script/procedures/p_spark.sql
+++ database/script/procedures/p_spark.sql
@@ -88,31 +88,27 @@ BEGIN
 
     SELECT *
     FROM (
-             SELECT detect_time                                         raised_time,
-                    line_code                                           line_code,
-                    direction                                           direction,
-                    position_code                                       position_code,
-                    locomotive_code                                     locomotive_code,
-                    count(DISTINCT locomotive_code)                     loco_cnt,
-                    sum(spark_cnt)                                      spark_cnt,
-                    sum(spark_tm)                                       spark_tm,
-                    round(sum(spark_tm) / nullif(sum(msc), 0) * 100, 5) spark_rate,
-                    sum(msc)                                            msc,
-                    max(spark_mx)                                       spark_mx,
-                 GROUPING (detect_time
-                     , line_code
-                     , direction
-                     , position_code
-                     , locomotive_code) dlevel
-                     , round(avg(avg_speed), 0) avg_speed,
-                 v_total_rows total_rows,
-                 cast(regexp_substr(GROUP_CONCAT(alarm_id ORDER BY spark_mx DESC SEPARATOR ','), '[^,]+') AS CHAR (100)) alarm_id
+             SELECT detect_time                                                                                            raised_time,
+                    line_code                                                                                              line_code,
+                    direction                                                                                              direction,
+                    position_code                                                                                          position_code,
+                    locomotive_code                                                                                        locomotive_code,
+                    count(DISTINCT locomotive_code)                                                                        loco_cnt,
+                    sum(spark_cnt)                                                                                         spark_cnt,
+                    sum(spark_tm)                                                                                          spark_tm,
+                    round(sum(spark_tm) / nullif(sum(msc), 0) * 100, 5)                                                    spark_rate,
+                    sum(msc)                                                                                               msc,
+                    max(spark_mx)                                                                                          spark_mx,
+                    GROUPING (detect_time , line_code , direction, position_code, locomotive_code)                         dlevel,
+                    round(avg(avg_speed), 0)                                                                               avg_speed,
+                    v_total_rows                                                                                           total_rows,
+                    cast(regexp_substr(GROUP_CONCAT(alarm_id ORDER BY spark_mx DESC SEPARATOR ','), '[^,]+') AS CHAR(100)) alarm_id
              FROM wv_sms_alarm_out a
              GROUP BY detect_time,
-                 line_code,
-                 direction,
-                 position_code,
-                 locomotive_code
+                      line_code,
+                      direction,
+                      position_code,
+                      locomotive_code
              WITH ROLLUP
              HAVING GROUPING (detect_time
                   , line_code
