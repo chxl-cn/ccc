37af683e64910b6df19ee0bdc9dc6df553a86da7
326682792@qq.com <326682792@qq.com>
2020-04-22 16:52:40 +0800

    delete dup alarm_id from alarm_aux

--- database/dataMigrate/initOpt/ORACLE/del_Dup_IDs.sql
+++ database/dataMigrate/initOpt/ORACLE/del_Dup_IDs.sql
@@ -1,22 +1,36 @@
-declare
-    type trec is record (aid varchar2(40),mri varchar2(60));
-    type tdr is table of trec ;
-    vdr tdr := tdr();
-begin
-    for p in (select PARTITION_NAME from USER_TAB_PARTITIONS p where p.TABLE_NAME = 'alarm_aux' order by p.PARTITION_POSITION desc)
-        loop
-            begin
-                execute immediate 'select alarm_id,min(ROWID) mrid from alarm_aux partition (' || p.PARTITION_NAME || ') group by alarm_id,having count(*) > 1'
-                    bulk collect into vdr;
-                for r in vdr.first .. vdr.LAST
-                    loop
-                        execute immediate 'delete from alarm_aux partition (' || p.PARTITION_NAME || ') where alarm_id=''' || vdr(r).aid || '''and rowid!=''' || vdr(r).mri ;
-                    end loop;
-                commit;
-            exception
-                when others then
-                    DBMS_OUTPUT.PUT_LINE(sqlerrm)
-            end;
+DECLARE
+    TYPE TR IS RECORD(
+        AID VARCHAR2(60),
+        RID VARCHAR2(60));
+    TYPE TID IS TABLE OF TR;
 
-        end loop;
-end;
+    VIDS TID := TID();
+    VRDS TID := TID();
+    S    CLOB;
+BEGIN
+    FOR P IN (SELECT PARTITION_NAME FROM USER_TAB_PARTITIONS P WHERE P.TABLE_NAME = 'ALARM_AUX' ORDER BY P.PARTITION_POSITION DESC)
+    LOOP
+        BEGIN
+
+            EXECUTE IMMEDIATE 'SELECT ALARM_ID, MIN(ROWID) MRID FROM ALARM_AUX PARTITION(' || P.PARTITION_NAME || ') GROUP BY ALARM_ID HAVING COUNT(*) > 1' BULK COLLECT
+                INTO VIDS;
+
+            CONTINUE WHEN VIDS.COUNT < 1;
+
+            FOR R IN VIDS.FIRST .. VIDS.LAST
+            LOOP
+                EXECUTE IMMEDIATE 'delete from alarm_aux partition (' || P.PARTITION_NAME || ') where alarm_id=''' || VIDS(R).AID || '''and rowid !=''' || VRDS(R).RID || '''';
+            END LOOP;
+
+            COMMIT;
+            DBMS_OUTPUT.PUT_LINE(P.PARTITION_NAME);
+        EXCEPTION
+
+            WHEN OTHERS THEN
+                ROLLBACK;
+                DBMS_OUTPUT.PUT_LINE(SQLERRM);
+        END;
+
+    END LOOP;
+END;
+/
