f6626ac9a046ea6cfb5f9264b49cb4e2668dbee1
326682792@qq.com <326682792@qq.com>
2020-04-22 11:10:21 +0800

    modify trigger

--- database/script/triggers/trg_alarm_mod_hist_inc.sql
+++ database/script/triggers/trg_alarm_mod_hist_inc.sql
@@ -8,17 +8,13 @@ CREATE TRIGGER `trg_alarm_mod_hist_inc`
     FOR EACH ROW
 trg_body:
 BEGIN
-    DECLARE v_loco VARCHAR(60);
-    DECLARE v_rdt DATETIME;
 
-    SET v_loco := OLD.detect_device_code;
-    IF v_loco IS NULL THEN
+    IF new.detect_device_code IS NULL THEN
         LEAVE trg_body;
     END IF;
 
-    SET v_rdt := cast(old.raised_time AS DATE);
 
     REPLACE alarm_hist_inc
-    SET running_date=v_rdt,
-        locomotive_code=v_loco;
+    SET running_date=date(new.old.raised_time),
+        locomotive_code=new.detect_device_code;
 END //
