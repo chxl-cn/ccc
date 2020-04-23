39d19c55d4b398987a4f9268862711de7ef8cba3
chuxl <326682792@qq.com>
2020-04-23 16:12:46 +0800

    add fields in section into

--- database/script/triggers/trg_sms_loco_lrt.sql
+++ database/script/triggers/trg_sms_loco_lrt.sql
@@ -1,5 +1,6 @@
 DELIMITER ;
 DROP TRIGGER IF EXISTS trg_sms_loco_lrt;
+
 DELIMITER //
 CREATE TRIGGER `trg_sms_loco_lrt`
     AFTER INSERT
@@ -7,17 +8,27 @@ CREATE TRIGGER `trg_sms_loco_lrt`
     FOR EACH ROW
 trg_body:
 BEGIN
-
     IF new.detect_time > now() THEN
         LEAVE trg_body;
     END IF;
 
-    INSERT INTO loco_lrt(locomotive_code, data_sort)
-    VALUES (new.locomotive_code, 2)
+
+    INSERT INTO loco_lrt( locomotive_code
+                        , data_sort
+                        , last_time
+                        , id
+                        , g_id
+                        , g_last_time)
+    VALUES ( new.locomotive_code
+           , 2
+           , new.detect_time
+           , new.id
+           , if(ifnull(new.valid_gps, 0) = 1, new.id, NULL)
+           , if(ifnull(new.valid_gps, 0) = 1, new.detect_time, NULL))
     ON DUPLICATE KEY
         UPDATE `last_time`   = if(`last_time` < new.`detect_time`, new.`detect_time`, `last_time`),
                `id`          = if(`last_time` < new.`detect_time`, new.`id`, `id`),
-               `g_id`        = if(`last_time` < new.`detect_time`, if(ifnull(NEW.`valid_gps`, 0), new.`id`, `g_id`), `g_id`),
-               `g_last_time` = if(`last_time` < new.`detect_time`, if(ifnull(NEW.`valid_gps`, 0), new.`detect_time`, `g_last_time`), `g_last_time`);
+               `g_id`        = if(`last_time` < new.`detect_time`, if(ifnull(NEW.`valid_gps`, 0) = 1, new.`id`, `g_id`), `g_id`),
+               `g_last_time` = if(`last_time` < new.`detect_time`, if(ifnull(NEW.`valid_gps`, 0) = 1, new.`detect_time`, `g_last_time`), `g_last_time`);
 
 END //
