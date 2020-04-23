07d46f4fa11c946916a81f366817c3ef267e2062
chuxl <326682792@qq.com>
2020-04-23 16:25:22 +0800

    call procedure p_loco_lrt_ins

--- database/script/triggers/trg_alarm_loco_lrt.sql
+++ database/script/triggers/trg_alarm_loco_lrt.sql
@@ -13,11 +13,5 @@ BEGIN
         LEAVE trg_body ;
     END IF;
 
-    INSERT INTO loco_lrt(locomotive_code, data_sort)
-    VALUES (new.detect_device_code, 1)
-    ON DUPLICATE KEY
-        UPDATE `last_time`   = if(`last_time` < new.`raised_time`, new.`raised_time`, `last_time`),
-               `id`          = if(`last_time` < new.`raised_time`, new.`id`, `id`),
-               `g_id`        = if(`last_time` < new.`raised_time`, if(ifnull(NEW.`valid_gps`, 0), new.`id`, `g_id`), `g_id`),
-               `g_last_time` = if(`last_time` < new.`raised_time`, if(ifnull(NEW.`valid_gps`, 0), new.`raised_time`, `g_last_time`), `g_last_time`);
+    CALL p_loco_lrt_ins(new.detect_device_code, 1, new.id, new.raised_time, new.valid_gps);
 END //
