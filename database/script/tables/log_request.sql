df99ecaab2a915acdb53852fc53b49ba54762003
chuxl <326682792@qq.com>
2020-04-24 16:57:40 +0800

    log_request fields

--- database/script/tables/log_request.sql
+++ database/script/tables/log_request.sql
@@ -1,14 +1,14 @@
 CREATE TABLE `log_request`
 (
-    `id`            varchar(23) NOT NULL,
-    `USER_NAME`     varchar(63),
-    `CLIENT_IP`     varchar(127),
-    `EVENT_ID`      varchar(31),
-    `SESSION_INFOS` varchar(255),
-    `URL`           varchar(511),
-    `MAIN_URL`      varchar(511),
-    `REQUEST_TIME`  datetime,
-    `COST_TIME`     int(11)
+    `id`            VARCHAR(63) NOT NULL,
+    `USER_NAME`     VARCHAR(127),
+    `CLIENT_IP`     VARCHAR(127),
+    `EVENT_ID`      VARCHAR(31),
+    `SESSION_INFOS` VARCHAR(511),
+    `URL`           VARCHAR(2047),
+    `MAIN_URL`      VARCHAR(511),
+    `REQUEST_TIME`  DATETIME,
+    `COST_TIME`     INT(11)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_0900_ai_ci;
