196423ff4ec68e222e7aee1b6345eb96359285f5
chuxl <326682792@qq.com>
2020-04-26 15:18:26 +0800

    load inc data since full load

--- database/dataMigrate/init_scripts/ora_to_mysv8/2_load_inc_data.sql
+++ database/dataMigrate/init_scripts/ora_to_mysv8/2_load_inc_data.sql
@@ -3,10 +3,12 @@
     1、提前时间点：xxxxxxx
     2、日期格式为 yyyymmdd (年月日)
 
+    注：在全量数据迁移之后的期间，本脚本所包含的模型不能有修改调整，否则，应在修正相应的分区表，同时修正查询列表字段
+
  */
 
 
-CREATE TABLE alarm_mg_part
+insert into alarm_mg_part
     PARTITION BY RANGE (raised_time)
     INTERVAL (INTERVAL '1' DAY)
 (
@@ -18,7 +20,7 @@ FROM alarm
 where raised_time >= to_date('xxxxxxxx', 'yyyymmdd')
 ;
 
-CREATE TABLE alarm_aux_mg_part
+insert into alarm_aux_mg_part
     PARTITION BY RANGE (raised_time_aux)
     INTERVAL (INTERVAL '1' DAY)
 (
@@ -31,7 +33,7 @@ WHERE raised_time_aux >= to_date('xxxxxxxx', 'yyyymmdd')
 ;
 
 
-CREATE TABLE alarm_img_data_mg_part
+insert into alarm_img_data_mg_part
     PARTITION BY RANGE (raise_time)
     INTERVAL (INTERVAL '1' DAY)
 (
@@ -44,7 +46,7 @@ WHERE raise_time >= to_date('xxxxxxxx', 'yyyymmdd')
 ;
 
 
-CREATE TABLE c3_sms_mg_part
+insert into c3_sms_mg_part
     PARTITION BY RANGE (detect_time)
     INTERVAL (INTERVAL '1' DAY)
 (
