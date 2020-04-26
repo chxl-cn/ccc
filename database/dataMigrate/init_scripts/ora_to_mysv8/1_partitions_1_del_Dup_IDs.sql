bb4a51deb8f6635609ed5e354c04383388e705b7
chuxl <326682792@qq.com>
2020-04-26 15:41:00 +0800

    delete duplicate alarm_id and specify date

--- database/dataMigrate/init_scripts/ora_to_mysv8/1_partitions_1_del_Dup_IDs.sql
+++ database/dataMigrate/init_scripts/ora_to_mysv8/1_partitions_1_del_Dup_IDs.sql
@@ -1,36 +1,47 @@
+/*
+  替换xxxxxxxx 为指定日期，其日期格式：yyyymmdd
+
+ */
+
 DECLARE
-    TYPE TR IS RECORD(
+    TYPE TR IS RECORD (
         AID VARCHAR2(60),
         RID VARCHAR2(60));
     TYPE TID IS TABLE OF TR;
 
-    VIDS TID := TID();
-    VRDS TID := TID();
-    S    CLOB;
+    VIDS       TID := TID();
+    VRDS       TID := TID();
+    v_sql      varchar2(1000) ;
+    v_old_date date ;
+    v_dyn_date date;
 BEGIN
-    FOR P IN (SELECT PARTITION_NAME FROM USER_TAB_PARTITIONS P WHERE P.TABLE_NAME = 'ALARM_AUX' ORDER BY P.PARTITION_POSITION DESC)
-    LOOP
-        BEGIN
+    v_old_date := to_date('xxxxxxxx', 'yyyymmdd');
+    FOR P IN (SELECT PARTITION_NAME, p.HIGH_VALUE FROM USER_TAB_PARTITIONS P WHERE P.TABLE_NAME = 'ALARM_AUX' ORDER BY P.PARTITION_POSITION DESC)
+        LOOP
+            execute immediate 'select ' || p.HIGH_VALUE || ' from dual ' into v_dyn_date;
+            continue when v_dyn_date >= v_old_date;
 
-            EXECUTE IMMEDIATE 'SELECT ALARM_ID, MIN(ROWID) MRID FROM ALARM_AUX PARTITION(' || P.PARTITION_NAME || ') GROUP BY ALARM_ID HAVING COUNT(*) > 1' BULK COLLECT
-                INTO VIDS;
+            BEGIN
+                v_sql := replace('SELECT ALARM_ID,MIN(ROWID) MRID FROM ALARM_AUX PARTITION( :PARTITION_NAME ) GROUP BY ALARM_ID HAVING COUNT(*) > 1', ':PARTITION_NAME', p.PARTITION_NAME);
 
-            CONTINUE WHEN VIDS.COUNT < 1;
+                EXECUTE IMMEDIATE v_sql BULK COLLECT INTO VIDS;
 
-            FOR R IN VIDS.FIRST .. VIDS.LAST
-            LOOP
-                EXECUTE IMMEDIATE 'delete from alarm_aux partition (' || P.PARTITION_NAME || ') where alarm_id=''' || VIDS(R).AID || '''and rowid !=''' || VRDS(R).RID || '''';
-            END LOOP;
+                CONTINUE WHEN VIDS.COUNT < 1;
 
-            COMMIT;
-            DBMS_OUTPUT.PUT_LINE(P.PARTITION_NAME);
-        EXCEPTION
+                FOR R IN VIDS.FIRST .. VIDS.LAST
+                    LOOP
+                        EXECUTE IMMEDIATE 'delete from alarm_aux partition (' || P.PARTITION_NAME || ') where alarm_id=''' || VIDS(R).AID || '''and rowid !=''' || VRDS(R).RID || '''';
+                    END LOOP;
 
-            WHEN OTHERS THEN
-                ROLLBACK;
-                DBMS_OUTPUT.PUT_LINE(SQLERRM);
-        END;
+                COMMIT;
+                DBMS_OUTPUT.PUT_LINE(P.PARTITION_NAME);
+            EXCEPTION
 
-    END LOOP;
+                WHEN OTHERS THEN
+                    ROLLBACK;
+                    DBMS_OUTPUT.PUT_LINE(SQLERRM);
+            END;
+
+        END LOOP;
 END;
 /
