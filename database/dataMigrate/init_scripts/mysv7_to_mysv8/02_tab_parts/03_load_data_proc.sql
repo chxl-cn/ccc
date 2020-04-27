f4e9b947b0f9f7c75038ac5189617d5edd3c4597
chuxl <326682792@qq.com>
2020-04-27 17:01:31 +0800

    add procedrue p_load_data_proc for load data

--- database/dataMigrate/init_scripts/mysv7_to_mysv8/02_tab_parts/03_load_data_proc.sql
+++ database/dataMigrate/init_scripts/mysv7_to_mysv8/02_tab_parts/03_load_data_proc.sql
@@ -1,13 +1,62 @@
-
 DELIMITER ;
-drop PROCEDURE IF EXISTS p_load_data_proc;
+DROP PROCEDURE IF EXISTS p_load_data_proc;
 
 
 /*
  p_sort :0 历史，1 增量
  */
 DELIMITER  //
-create PROCEDURE p_load_data_proc(p_date DATETIME,p_sort TINYINT)
+CREATE PROCEDURE p_load_data_proc(p_date  DATETIME
+                                 , p_sort TINYINT
+                                 )
 BEGIN
+
+
+    IF p_sort = 0 THEN
+        INSERT INTO alarm_aux_pold
+        SELECT *
+        FROM alarm_aux x
+        WHERE x.raised_time_aux < p_date;
+
+        INSERT INTO alarm_img_data_pold
+        SELECT *
+        FROM alarm_img_data d
+        WHERE d.raise_time < p_date;
+
+        INSERT INTO nos_aa_pnew
+        SELECT *
+        FROM nos_aa a
+        WHERE a.INPUTDATE < p_date;
+
+        INSERT INTO nos_ac_pnew
+        SELECT *
+        FROM nos_ac c
+        WHERE c.INPUTDATE < p_date;
+    ELSE
+        INSERT INTO alarm_aux_pold
+        SELECT *
+        FROM alarm_aux x
+        WHERE x.raised_time_aux >= p_date;
+
+        INSERT INTO alarm_img_data_pold
+        SELECT *
+        FROM alarm_img_data d
+        WHERE d.raise_time >= p_date;
+
+        INSERT INTO nos_aa_pnew
+        SELECT *
+        FROM nos_aa a
+        WHERE a.INPUTDATE >= p_date;
+
+        INSERT INTO nos_ac_pnew
+        SELECT *
+        FROM nos_ac c
+        WHERE c.INPUTDATE >= p_date;
+
+    END IF;
+
+    LOOP
+        ;
+    END LOOP ;
 
 END //
