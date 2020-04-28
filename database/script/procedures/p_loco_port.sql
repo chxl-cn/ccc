ad8a03394572a36ce6c72bea42b6502625cb1d75
chuxl <326682792@qq.com>
2020-04-28 11:45:30 +0800

    p_loco_port

--- database/script/procedures/p_loco_port.sql
+++ database/script/procedures/p_loco_port.sql
@@ -8,7 +8,13 @@ CREATE PROCEDURE p_loco_port(p_loco       VARCHAR(40)
                             , OUT p_port2 VARCHAR(5)
                             )
 BEGIN
-    SELECT regexp_substr(l.device_bow_relations, '[[:digit:]]+,[[:digit:]]+', 1, p_gno) FROM locomotive l WHERE l.locomotive_code = p_loco ;
+    SELECT regexp_substr(p, '[[:digit:]]+', 1, 1), regexp_substr(p, '[[:digit:]]+', 1, 2)
+    INTO p_port1,p_port2
+    FROM (
+             SELECT regexp_substr(l.device_bow_relations, '[[:digit:]]+,[[:digit:]]+', 1, p_gno) p
+             FROM locomotive l
+             WHERE l.locomotive_code = p_loco
+         ) t;
 END //
 
 
