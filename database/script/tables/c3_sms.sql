3a76eb8252840de4db64af208e15dc0d6801921f
chuxl <326682792@qq.com>
2020-04-28 15:46:50 +0800

    tax_monitor_status`  VARCHAR(16)

--- database/script/tables/c3_sms.sql
+++ database/script/tables/c3_sms.sql
@@ -29,7 +29,7 @@ CREATE TABLE `c3_sms`
     `eoas_location`       INT(11),
     `eoas_time`           DATETIME,
     `pos_confirmed`       VARCHAR(1),
-    `tax_monitor_status`  VARCHAR(8),
+    `tax_monitor_status`  VARCHAR(16),
     `tax_position`        VARCHAR(10),
     `tax_schedule_status` VARCHAR(10),
     `version`             VARCHAR(8),
