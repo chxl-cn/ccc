75445f79903eaf9e07b1ad36b15855997faff6f8
chuxl <326682792@qq.com>
2020-04-28 11:39:43 +0800

    p_loco_port

--- database/script/procedures/p_loco_port.sql
+++ database/script/procedures/p_loco_port.sql
@@ -1,11 +1,15 @@
 DELIMITER  ;
-drop PROCEDURE IF EXISTS p_loco_port;
+DROP PROCEDURE IF EXISTS p_loco_port;
 
 DELIMITER  //
-CREATE PROCEDURE p_loco_port()
+CREATE PROCEDURE p_loco_port(p_loco       VARCHAR(40)
+                            , p_gno       VARCHAR(2)
+                            , OUT p_port1 VARCHAR(5)
+                            , OUT p_port2 VARCHAR(5)
+                            )
 BEGIN
-    ;
-end //
+    SELECT regexp_substr(l.device_bow_relations, '[[:digit:]]+,[[:digit:]]+', 1, p_gno) FROM locomotive l WHERE l.locomotive_code = p_loco ;
+END //
 
 
 
