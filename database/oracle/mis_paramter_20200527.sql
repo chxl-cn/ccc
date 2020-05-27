d51d4adfc191958f45044214c0ef3f28e9142d68
xuyong <cdxuyong@qq.com>
2020-05-27 09:47:42 +0800

    docs:对内数据库增加数据库脚本

---
+++ database/oracle/mis_paramter_20200527.sql
@@ -0,0 +1,3 @@
+
+insert into mis_paramter (ID, KEY, VALUE, TITILE, CONTEXT, TIME, ROWID)
+values ('C3Parser.Public.013', 'AlarmParser.ChannelNum', '32', '解析服务调用技术部dll通道数量', 'C3Parser.Public', null, null);
