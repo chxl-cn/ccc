3ba643a951e833d1d68998968e52d00d23b0783f
chuxl <326682792@qq.com>
2020-06-03 18:11:59 +0800

    comfirm v8.1

--- database/V8.0/dataMigrate/init_scripts/mysv7_to_mysv8/02_tab_parts/03_load_data_proc.sql
+++ database/V8.0/dataMigrate/init_scripts/mysv7_to_mysv8/02_tab_parts/03_load_data_proc.sql
@@ -624,7 +624,7 @@ BEGIN
 
 
             SET v_outfile = replace(v_goutfile, ':file:', concat('c3_sms_monitor_pnew_', date(v_ed), '_', p_sort));
-            SET v_outfile = replace(v_outfile, ':file:', 'c3_sms_monitor');
+            SET v_outfile = replace(v_outfile, ':dir:', 'c3_sms_monitor');
             SET v_sql = concat(v_sms_sql, char(10), v_outfile);
             SET @c3_sms_monitor_pnew = v_sql;
             PREPARE stmt_c3_sms_monitor_pnew FROM @c3_sms_monitor_pnew;
