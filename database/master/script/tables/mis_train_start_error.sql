bc24e1849ecefa7fafab93110c9d93c9fe60051c
chuxl <326672892@qq.com>
2020-06-05 12:00:09 +0800

    mis_train_start_error

---
+++ database/master/script/tables/mis_train_start_error.sql
@@ -0,0 +1,8 @@
+CREATE TABLE mis_train_start_error
+( train_number VARCHAR(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin  ,
+  dttime       DATETIME
+)
+    ENGINE = InnoDB
+    DEFAULT CHARSET = utf8mb4
+    COLLATE = utf8mb4_bin
+;
